4af441af0a4df33d46fd9a6e3df09475244cc194
JawadBLK <jawad5926@gmail.com>
2024-06-12 18:07:32 +0200

    Test unitaires avant refacto

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -1,14 +1,66 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class AlarmTest {
+
+    private class TestSensor extends Sensor {
+        private double pressure;
+    
+    public void setPressure(double pressure) {
+            this.pressure = pressure;
+        }
+
+    @Override
+    public double popNextPressurePsiValue() {
+        return pressure;
+       }
+    }
 
     @Test
     public void foo() {
         Alarm alarm = new Alarm();
         assertFalse(alarm.isAlarmOn());
     }
+
+
+    @Test
+    void testAlarmOnLowPressure() {
+        TestSensor testSensor = new TestSensor();
+        testSensor.setPressure(16); 
+        Alarm alarm = new Alarm();
+        alarm.sensor = testSensor;
+
+        alarm.check();
+
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    @Test
+    void testAlarmOnHighPressure() {
+        TestSensor testSensor = new TestSensor();
+        testSensor.setPressure(22); 
+        Alarm alarm = new Alarm();
+        alarm.sensor = testSensor;
+
+        alarm.check();
+
+        assertTrue(alarm.isAlarmOn());
+    }
+
+    @Test
+    void testAlarmOffForNormalPressure() {
+        TestSensor testSensor = new TestSensor();
+        testSensor.setPressure(18); 
+        Alarm alarm = new Alarm();
+        alarm.sensor = testSensor;
+
+        alarm.check();
+
+        assertFalse(alarm.isAlarmOn());
+    }
+    
 }
